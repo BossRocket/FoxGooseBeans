026eb456dccb0caf3880c0b7904ff9c15d4679f4
Patrick Maley <patrickrmaley@gmail.com>
2021-02-20 19:24:17 -0500

    Fixed logic, program is now finished and works as intended

--- FoxGooseBeans.java
+++ FoxGooseBeans.java
@@ -1,6 +1,3 @@
-// Currently a work-in-progress. While this compiles properly, it doesn't output an actual
-// solution to the problem.
-
 // This is a brief backtracking solution to the fox, goose, and bag of beans problem.
 // Read about it here: https://en.wikipedia.org/wiki/Wolf,_goat_and_cabbage_problem
 // In particular, this solution uses recursive depth-first search.
@@ -9,27 +6,21 @@ import java.util.*;
 
 public class FoxGooseBeans
 {
-	// boolean array to represent the positions of the farmer, fox, goose, and beans
-	// ^in that order
-	// false = north, true = south
+	// boolean array to represent the positions of the farmer, fox, goose, and beans (in that order)
 	static boolean positions[] = new boolean[4];
 
-	// HashSet for keeping track of previous states
 	static HashSet<Integer> states = new HashSet<>();
 
-	// Small arraylist to hold the order in which we moved things around.
 	static ArrayList<String> moves = new ArrayList<>();
 
-	// Method to add the contents of the array the HashSet
 	public static boolean hashThese(boolean positions[])
 	{
 		int temp = 0;
 
+		// For each value in 'positions', this creates a unique value based on their combined value.
 		for (int i = 0; i < positions.length; i++)
-		{
 			if (positions[i])
 				temp += (i + 1) * (i + 1);
-		}
 
 		return states.add(temp);
 	}
@@ -37,12 +28,25 @@ public class FoxGooseBeans
 	public static void printMoves()
 	{
 		for (int i = moves.size() - 1; i >= 0; i--)
-		{
 			System.out.println(moves.get(i));
+	}
+
+	// Moves the farmer and farm-thing (if applicable) to the opposite side
+	public static void setPositions(int i)
+	{
+		if (positions[i] == true)
+		{
+			positions[i] = false;
+			positions[0] = false;
+		}
+		else
+		{
+			positions[i] = true;
+			positions[0] = true;
 		}
 	}
 
-	// Recursive DFS
+	// Recursive DFS method
 	public static boolean findRoute(boolean positions[])
 	{
 		// Base case
@@ -55,60 +59,53 @@ public class FoxGooseBeans
 
 		for (int i = 0; i < positions.length; i++)
 		{
-			if (positions[i] == true)
-				positions[i] = false;
-			else
-				positions[i] = true;
+			// If the farmer is not on the side of i, nothing can happen.
+			if (positions[0] != positions[i])
+				continue;
 
+			// Try moving positions.
+			setPositions(i);
+
+			// Revert if these positions are bad.
 			if (positions[1] == positions[2] && positions[1] != positions[0])
 			{
-				if (positions[i] == true)
-					positions[i] = false;
-				else
-					positions[i] = true;
-
+				setPositions(i);
 				continue;
 			}
 
 			if (positions[2] == positions[3] && positions[2] != positions[0])
 			{
-				if (positions[i] == true)
-					positions[i] = false;
-				else
-					positions[i] = true;
-
+				setPositions(i);
 				continue;
 			}
-
-			// Move the farmer with the farm things
-			if (i != 0)
-				positions[0] = positions[i];
 
 			if (findRoute(positions))
 			{
 				if (i != 0)
 				{
+					positions[0] = positions[i];
+
 					if (i == 1)
-						moves.add("Move Fox");
+						moves.add("Move Farmer and Fox");
 
 					if (i == 2)
-						moves.add("Move Goose");
+						moves.add("Move Farmer and Goose");
 
 					if (i == 3)
-						moves.add("Move Beans");
-
-					moves.add("Move Farmer and");
+						moves.add("Move Farmer and Beans");
 				}
 
 				else
 				{
-					if (i == 0)
-					{
-						moves.add("Move Farmer");
-					}
+					moves.add("Move Farmer");
 				}
 
 				return true;
+			}
+
+			else
+			{
+				setPositions(i);
 			}
 		}
 
